4c87030dadfd58825b678822ab72dd0405cab708
tegraA <tegraA@example.com>
2018-06-05 15:18:41 -0400

    looks like I already add ar2031 support in master branch... keep using it for porting

--- NVIDIA/samples/nvmedia/img_cap_eglprod/save.c
+++ NVIDIA/samples/nvmedia/img_cap_eglprod/save.c
@@ -30,6 +30,13 @@
             (pSrcBuff[srcPitch*(y + yOffset) + 2*(x + xOffset) + 1] << 2) | \
             (pSrcBuff[srcPitch*(y + yOffset) + 2*(x + xOffset)] >> 6)
 
+enum PixelColor {
+    RED,
+    GREEN1,
+    GREEN2,
+    BLUE,
+    NUM_PIXEL_COLORS
+};
 
 static void
 _CreateOutputFileName(
@@ -49,6 +56,182 @@ _CreateOutputFileName(
     sprintf(buf, "%02d", frame);
     strcat(outputFileName, buf);
     strcat(outputFileName, ".raw");
+}
+
+static NvMediaStatus
+_ConvGetPixelOffsets(NvMediaRawPixelOrder pixelOrder,
+                     NvU32 *xOffsets,
+                     NvU32 *yOffsets)
+{
+    if (!xOffsets || !yOffsets)
+        return NVMEDIA_STATUS_BAD_PARAMETER;
+
+    switch (pixelOrder) {
+        case NVMEDIA_RAW_PIXEL_ORDER_RGGB:
+            CONV_GET_X_OFFSET(xOffsets, RED, GREEN1, GREEN2, BLUE);
+            CONV_GET_Y_OFFSET(yOffsets, RED, GREEN1, GREEN2, BLUE);
+            break;
+        case NVMEDIA_RAW_PIXEL_ORDER_GRBG:
+            CONV_GET_X_OFFSET(xOffsets, GREEN1, RED, BLUE, GREEN2);
+            CONV_GET_Y_OFFSET(yOffsets, GREEN1, RED, BLUE, GREEN2);
+            break;
+        case NVMEDIA_RAW_PIXEL_ORDER_GBRG:
+            CONV_GET_X_OFFSET(xOffsets, GREEN1, BLUE, RED, GREEN2);
+            CONV_GET_Y_OFFSET(yOffsets, GREEN1, BLUE, RED, GREEN2);
+            break;
+        case NVMEDIA_RAW_PIXEL_ORDER_BGGR:
+        default:
+            CONV_GET_X_OFFSET(xOffsets, BLUE, GREEN1, GREEN2, RED);
+            CONV_GET_Y_OFFSET(yOffsets, BLUE, GREEN1, GREEN2, RED);
+            break;
+    }
+
+    return NVMEDIA_STATUS_OK;
+}
+
+static NvMediaStatus
+_ConvRawToRgba(NvMediaImage *imgSrc,
+               NvMediaImage *imgDst,
+               NvU32 rawBytesPerPixel,
+               NvU32 pixelOrder)
+{
+    NvMediaImageSurfaceMap surfaceMap;
+    NvU32 srcImageSize = 0, srcWidth, srcHeight;
+    NvU32 dstImageSize = 0, dstWidth, dstHeight;
+    NvU8 *pSrcBuff = NULL, *pDstBuff = NULL, *pTmp = NULL;
+    NvU32 srcPitch = 0, dstPitch = 0;
+    NvMediaStatus status;
+    NvU8 alpha = 0xFF;
+    NvU32 x = 0, y = 0;
+    NvU32 xOffsets[NUM_PIXEL_COLORS] = {0}, yOffsets[NUM_PIXEL_COLORS] = {0};
+    NVM_SURF_FMT_DEFINE_ATTR(srcAttr);
+    NVM_SURF_FMT_DEFINE_ATTR(dstAttr);
+
+    status = NvMediaSurfaceFormatGetAttrs(imgSrc->type,
+                                    srcAttr,
+                                    NVM_SURF_FMT_ATTR_MAX);
+    if (status != NVMEDIA_STATUS_OK) {
+        LOG_ERR("%s:NvMediaSurfaceFormatGetAttrs failed\n", __func__);
+        return NVMEDIA_STATUS_ERROR;
+    }
+
+    status = NvMediaSurfaceFormatGetAttrs(imgDst->type,
+                                    dstAttr,
+                                    NVM_SURF_FMT_ATTR_MAX);
+    if (status != NVMEDIA_STATUS_OK) {
+        LOG_ERR("%s:NvMediaSurfaceFormatGetAttrs failed\n", __func__);
+        return NVMEDIA_STATUS_ERROR;
+    }
+
+    if (NvMediaImageLock(imgSrc, NVMEDIA_IMAGE_ACCESS_WRITE, &surfaceMap) !=
+        NVMEDIA_STATUS_OK) {
+        LOG_ERR("%s: NvMediaImageLock failed\n", __func__);
+        return NVMEDIA_STATUS_ERROR;
+    }
+
+    srcHeight = surfaceMap.height;
+    srcWidth  = surfaceMap.width;
+
+    if (srcAttr[NVM_SURF_ATTR_SURF_TYPE].value == NVM_SURF_ATTR_SURF_TYPE_RAW) {
+        srcPitch = srcWidth * rawBytesPerPixel;
+        srcImageSize = srcPitch * srcHeight;
+        srcImageSize += imgSrc->embeddedDataTopSize;
+        srcImageSize += imgSrc->embeddedDataBottomSize;
+    } else {
+        LOG_ERR("%s: Unsupported source surface type\n", __func__);
+        return NVMEDIA_STATUS_ERROR;
+    }
+
+    if (!(pSrcBuff = malloc(srcImageSize))) {
+        LOG_ERR("%s: Out of memory\n", __func__);
+        return NVMEDIA_STATUS_OUT_OF_MEMORY;
+    }
+
+    status = NvMediaImageGetBits(imgSrc, NULL, (void **)&pSrcBuff, &srcPitch);
+    NvMediaImageUnlock(imgSrc);
+    if (status != NVMEDIA_STATUS_OK) {
+        LOG_ERR("%s: NvMediaImageGetBits() failed\n", __func__);
+        goto done;
+    }
+
+    dstHeight = srcHeight / 2;
+    dstWidth  = srcWidth / 2;
+
+    if (dstAttr[NVM_SURF_ATTR_SURF_TYPE].value == NVM_SURF_ATTR_SURF_TYPE_RGBA) {
+        dstPitch = dstWidth * 4;
+        dstImageSize = dstHeight * dstPitch;
+    } else {
+        LOG_ERR("%s: Unsupported destination surface type\n", __func__);
+        status = NVMEDIA_STATUS_ERROR;
+        goto done;
+    }
+
+    if (!(pDstBuff = calloc(1, dstImageSize))) {
+        LOG_ERR("%s: Out of memory\n", __func__);
+        status = NVMEDIA_STATUS_OUT_OF_MEMORY;
+        goto done;
+    }
+
+    pTmp = pDstBuff;
+    /* Convert to rgba */
+
+    /* Y is starting at valid pixel, skipping embedded lines from top */
+    y = imgSrc->embeddedDataTopSize / srcPitch;
+
+    /* Get offsets for each pixel color */
+    status = _ConvGetPixelOffsets(pixelOrder, xOffsets, yOffsets);
+    if (status != NVMEDIA_STATUS_OK) {
+        LOG_ERR("%s: Failed to get PixelOffsets\n", __func__);
+        return status;
+    }
+
+    if (srcAttr[NVM_SURF_ATTR_BITS_PER_COMPONENT].value == NVM_SURF_ATTR_BITS_PER_COMPONENT_12) {
+        for (; y < srcHeight; y += 2) {
+            for (x = 0; x < srcWidth; x += 2) {
+                /* R */
+                *pTmp = CONV_CALCULATE_PIXEL(pSrcBuff, srcPitch, x, y, xOffsets[RED], yOffsets[RED]);
+                pTmp++;
+                /* G (average of green in BGGR) */
+                *pTmp = ((CONV_CALCULATE_PIXEL(pSrcBuff, srcPitch, x, y, xOffsets[GREEN1], yOffsets[GREEN1])) +
+                         (CONV_CALCULATE_PIXEL(pSrcBuff, srcPitch, x, y, xOffsets[GREEN2], yOffsets[GREEN2]))) /2 ;
+                pTmp++;
+                /* B */
+                *pTmp = CONV_CALCULATE_PIXEL(pSrcBuff, srcPitch, x, y, xOffsets[BLUE], yOffsets[BLUE]);
+                pTmp++;
+                /* A */
+                *pTmp = alpha;
+                pTmp++;
+            }
+        }
+    } else {
+        LOG_ERR("%s: Unsupported input raw format\n", __func__);
+        status = NVMEDIA_STATUS_ERROR;
+        goto done;
+    }
+    memset(&surfaceMap, 0, sizeof(surfaceMap));
+
+    if (NvMediaImageLock(imgDst, NVMEDIA_IMAGE_ACCESS_WRITE, &surfaceMap) !=
+       NVMEDIA_STATUS_OK) {
+        LOG_ERR("%s: NvMediaImageLock failed\n", __func__);
+        status = NVMEDIA_STATUS_ERROR;
+        goto done;
+    }
+
+    status = NvMediaImagePutBits(imgDst, NULL, (void **)&pDstBuff, &dstPitch);
+    NvMediaImageUnlock(imgDst);
+    if (status != NVMEDIA_STATUS_OK) {
+        LOG_ERR("%s: NvMediaImagePutBits() failed\n", __func__);
+        goto done;
+    }
+
+    status = NVMEDIA_STATUS_OK;
+done:
+    if (pSrcBuff)
+        free(pSrcBuff);
+    if (pDstBuff)
+        free(pDstBuff);
+
+    return status;
 }
 
 static uint32_t
@@ -198,8 +381,10 @@ SaveInit(
         LOG_ERR("%s:NvMediaSurfaceFormatGetAttrs failed\n", __func__);
         goto failed;
     }
-    saveCtx->threadCtx.width =  captureCtx->threadCtx[0].width;
-    saveCtx->threadCtx.height = captureCtx->threadCtx[0].height;
+    saveCtx->threadCtx[i].width =  (attr[NVM_SURF_ATTR_SURF_TYPE].value == NVM_SURF_ATTR_SURF_TYPE_RAW )?
+                                       captureCtx->threadCtx[i].width/2 : captureCtx->threadCtx[i].width;
+    saveCtx->threadCtx[i].height = (attr[NVM_SURF_ATTR_SURF_TYPE].value == NVM_SURF_ATTR_SURF_TYPE_RAW )?
+                                       captureCtx->threadCtx[i].height/2 : captureCtx->threadCtx[i].height;
     if (NvQueueCreate(&saveCtx->threadCtx.inputQueue,
                     SAVE_QUEUE_SIZE,
                     sizeof(NvMediaImage *)) != NVMEDIA_STATUS_OK) {
